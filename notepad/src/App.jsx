1d462c062d049f7b4a928d613627d3939123dde4
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-06-30 02:41:27 +0100

    notepad section added

--- notepad/src/App.jsx
+++ notepad/src/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="h-screen text-yellow-600 flex ">
+      <div className="h-screen w-1/2 flex flex-col gap-4 border-r-2 p-8">
+        <h1 className="text-3xl font-bold ">
+          NOTEPAD <span>☀️</span>
+        </h1>
+        <input
+          className="border-2 outline-none py-2 px-4 rounded"
+          type="text"
+          placeholder="Enter Notes Heading..."
+        />
+        <textarea
+          className="border-2 outline-none p-4 rounded h-40"
+          placeholder="Write Details here"
+        />
+        <button
+          className="py-2 px-4 border-none rounded-lg 
+        bg-yellow-600 text-white font-bold cursor-pointer active:scale-95"
+        >
+          Add Note
+        </button>
+      </div>
+      <div className="w-1/2 h-screen text-yellow-600 p-8">
+        <h1 className="text-3xl font-bold">Recent Notes</h1>
+        <div className="pt-2">
+          <div className="h-70 w-50 bg-[url('')] bg-cover"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
